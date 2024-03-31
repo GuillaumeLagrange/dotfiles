631bd07605aa219d0dd1abc5f5476f0f6000b373
Guillaume Lagrange <guillaume@glagrange.eu>
2024-03-31 14:46:31 +0200

    feat: start using nix for home packages

--- home.nix
+++ home.nix
@@ -1,12 +1,15 @@
 { config, pkgs, ... }:
 
-let nixGL = import ./nixGL.nix { inherit pkgs config; }; in
+let
+  nixGL = import ./nixGL.nix { inherit pkgs config; }; 
+  pkgsUnstable = import <nixpkgs-unstable> {};
+in
 {
-imports = [./options.nix];
+  imports = [ ./options.nix ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "glagrange";
-  home.homeDirectory = "/home/glagrange";
+  home.username = "guillaume";
+  home.homeDirectory = "/home/guillaume";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -23,6 +26,12 @@ imports = [./options.nix];
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     pkgs.hello
+    pkgs.wev
+    pkgs.grim
+    pkgs.swappy
+    pkgs.slurp
+    pkgs.fswatch
+    pkgs.telegram-desktop
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -45,6 +54,9 @@ imports = [./options.nix];
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    ".config/swappy/config" = { text = builtins.readFile ./swappy.conf; };
+    ".config/wezterm/wezterm.lua" = { text = builtins.readFile ./wezterm.lua; };
+
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
@@ -58,10 +70,7 @@ imports = [./options.nix];
   # Manager then you have to manually source 'hm-session-vars.sh' located at
   # either
   #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh or
   #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
   #
   # or
@@ -75,24 +84,238 @@ imports = [./options.nix];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Nvim
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
-    }))
-  ];
+  # Zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "bira";
+      plugins = [
+        "git"
+	"sudo"
+	"npm"
+	"docker"
+	"rust"
+	"systemd"
+      ];
+    };
 
+    initExtra = ''
+      bindkey '^ ' autosuggest-accept
+      alias insomnia-gen="ssh charybdis 'source ~/.zshrc && cdr dev_tools/InsomniaConfig && cargo run --release -- --certs-path /home/glagrange/stockly/Main/StocklyContinuousDeployment/certificates' && scp charybdis:stockly/Main/dev_tools/InsomniaConfig/insomnia_collection.json ~/"
+    '';
+  };
+
+  programs.direnv.enable = true;
+  programs.fzf.enable = true;
+
+  programs.git = {
+    userEmail = "guillaume@glagrange.eu";
+    userName = "Guillaume Lagrange";
+  };
+
+  # Nvim
   programs.neovim = {
     enable = true;
-    package = pkgs.neovim-nightly;
+    viAlias = true;
+    vimAlias = true;
+    package = pkgsUnstable.neovim-unwrapped;
     defaultEditor = true;
   };
 
-  # Shit doesnt work
-  programs.wezterm = {
+  wayland.windowManager.hyprland = {
     enable = true;
-    enableZshIntegration = true;
-    package = (nixGL pkgs.wezterm);
-    extraConfig = builtins.readFile ./wezterm.lua;
+    extraConfig = builtins.readFile ./hyprland.conf;
   };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    extraConfig = builtins.readFile ./sway.config;
+  };
+
+  programs.waybar = {
+    enable = true;
+    style = builtins.readFile ./style.css;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "bottom";
+        height = 22;
+        output = [ "*" ];
+        modules-left = [ "sway/workspaces" "sway/window" "sway/mode" ];
+        modules-center = [ ];
+        modules-right = [ "mpris" "tray" "disk" "cpu" "memory" "battery" "pulseaudio" "clock" ];
+
+        "hyprland/workspaces" = {
+          all-outputs = false;
+          show-special = true;
+        };
+
+        "hyprland/window" = {
+          separate-outputs = true;
+        };
+
+        "mpris" = {
+          "format" = " {player_icon} {status_icon} {dynamic} ";
+          "ignored-players" = [ ];
+          "player-icons" = {
+            "default" = " ";
+            "spotify" = " ";
+            "firefox" = " ";
+          };
+          "status-icons" = {
+            "paused" = " ";
+            "playing" = " ";
+          };
+          "dynamic-order" = [ "title" "artist" "position" "length" ];
+          "dynamic-len" = 70;
+          "interval" = 1;
+        };
+
+        "tray" = {
+          icon-size = 14;
+          spacing = 8;
+          show-passive-items = true;
+        };
+
+
+        "disk" = {
+          "format" = "{free}";
+        };
+
+        "pulseaudio" = {
+          "format" = "{icon} {volume}%";
+          "format-bluetooth" = " {icon} {volume}%";
+          "format-muted" = "󰝟  {volume}%";
+          "format-icons" = {
+            "default" = [ "󰕿 " "󰖀 " "󰕾 " ];
+          };
+          "scroll-step" = 1;
+          "on-click" = "pavucontrol";
+        };
+
+        "battery" = {
+          "format" = "{icon} {capacity}%";
+          "format-charging" = "󱐋 {icon} {capacity}%";
+          "format-icons" = [ "󰂎" "󱊡" "󱊢" "󱊣" ];
+        };
+
+        "clock" = {
+          "format" = "  {:L%H:%M} ";
+          "tooltip-format" = "<tt><small>{calendar}</small></tt>";
+          "calendar" = {
+            "mode" = "year";
+            "mode-mon-col" = 2;
+            "weeks-pos" = "left";
+            "on-scroll" = 1;
+            "on-click-right" = "mode";
+            "format" = {
+              "months" = "<span color='#ffead3'><b>{}</b></span>";
+              "days" = "<span color='#ecc6d9'><b>{}</b></span>";
+              "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
+              "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
+              "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          "actions" = {
+            "on-click-right" = "mode";
+            "on-click-forward" = "tz_up";
+            "on-click-backward" = "tz_down";
+            "on-scroll-up" = "shift_up";
+            "on-scroll-down" = "shift_down";
+          };
+        };
+      };
+    };
+  };
+
+  # Doesnt work
+  # programs.wezterm = {
+  #   enable = true;
+  #   # enableZshIntegration = true;
+  #   package = (nixGL pkgs.wezterm);
+  #   extraConfig = builtins.readFile ./wezterm.lua;
+  # };
+
+  programs.alacritty = {
+    enable = true;
+    settings = { };
+  };
+
+  programs.wofi.enable = true;
+
+  programs.wpaperd = {
+    enable = true;
+    settings = {
+      default = {
+        path = ~/documents/wallpapers;
+        duration = "1h";
+      };
+    };
+  };
+
+  services.cliphist.enable = true;
+
+  services.swayidle = {
+    enable = true;
+    timeouts =
+      let
+        lockTimeout = 60 * 10; # 10 minutes
+        screenOffTimeout = 10;
+        suspendTimeout = 2 * lockTimeout;
+        screenOffCommand = "swaymsg 'output * dpms off'";
+        screenOnCommand = "swaymsg 'output * dpms on'";
+      in
+      [
+        {
+          timeout = lockTimeout;
+          command = "PATH=/usr/bin ~/scripts/lock.sh&";
+        }
+        {
+          timeout = lockTimeout + screenOffTimeout;
+          command = "export PATH=/usr/bin && if pgrep -x swaylock; then ${screenOffCommand}; fi";
+          resumeCommand = "PATH=/usr/bin ${screenOnCommand}";
+        }
+        {
+          timeout = screenOffTimeout;
+          command = "export PATH=/usr/bin && if pgrep -x swaylock; then ${screenOffCommand}; fi";
+          resumeCommand = "PATH=/usr/bin ${screenOnCommand}";
+        }
+        {
+          timeout = suspendTimeout;
+          command = "PATH=/usr/bin systemctl suspend";
+        }
+      ];
+  };
+
+  systemd.user = {
+    enable = true;
+    startServices = true;
+    services = {
+      cliphist-wipe = {
+        Unit = {
+          Description = "Wipe cliphist at midnight";
+        };
+        Service = {
+          ExecStart = "${pkgs.cliphist}/bin/cliphist wipe";
+        };
+      };
+    };
+    timers = {
+      cliphist-wipe = {
+        Unit = {
+          Description = "Run cliphist wipe daily at midnight";
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
+        };
+        Timer = {
+          OnCalendar = "*-*-* 00:00:00";
+          Persistent = true;
+        };
+      };
+    };
+  };
+
 }
