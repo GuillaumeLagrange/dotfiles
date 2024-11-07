69eef83f1fea4019c0dec8245e4ccbb2b2e44022
Guillaume Lagrange <guillaume@glagrange.eu>
2024-11-07 23:32:58 +0100

    chore: move waybar config to its own file

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -13,12 +13,14 @@
     ./hyprland.nix
     ./sway.nix
     ./firefox.nix
+    ./waybar.nix
   ];
 
   config = lib.mkIf config.gui.enable {
     hyprland.enable = true;
     sway.enable = true;
     firefox.enable = true;
+    waybar.enable = true;
 
     home.packages = with pkgs; [
       telegram-desktop
@@ -263,130 +265,6 @@
           Timer = {
             OnCalendar = "*-*-* 00:00:00";
             Persistent = true;
-          };
-        };
-      };
-    };
-
-    programs.waybar = {
-      enable = true;
-      style = builtins.readFile ./style.css;
-      settings = {
-        mainBar = {
-          layer = "top";
-          position = "bottom";
-          height = 22;
-          output = [ "*" ];
-          modules-left = [
-            "hyprland/submap"
-            "hyprland/workspaces"
-            "hyprland/window"
-            "sway/workspaces"
-            "sway/window"
-            "sway/mode"
-          ];
-          modules-center = [ ];
-          modules-right = [
-            "mpris"
-            "tray"
-            "disk"
-            "cpu"
-            "memory"
-            "battery"
-            "pulseaudio"
-            "clock"
-          ];
-
-          "hyprland/workspaces" = {
-            all-outputs = false;
-            show-special = true;
-          };
-
-          "hyprland/window" = {
-            separate-outputs = true;
-          };
-
-          "mpris" = {
-            "format" = " {player_icon} {status_icon} {dynamic} ";
-            "ignored-players" = [ ];
-            "player-icons" = {
-              "default" = " ";
-              "spotify" = " ";
-              "firefox" = " ";
-            };
-            "status-icons" = {
-              "paused" = " ";
-              "playing" = " ";
-            };
-            "dynamic-order" = [
-              "title"
-              "artist"
-              "position"
-              "length"
-            ];
-            "dynamic-len" = 70;
-            "interval" = 1;
-          };
-
-          "tray" = {
-            icon-size = 14;
-            spacing = 8;
-            show-passive-items = true;
-          };
-
-          "disk" = {
-            "format" = "{free}";
-          };
-
-          "pulseaudio" = {
-            "format" = "{icon} {volume}%";
-            "format-bluetooth" = " {icon} {volume}%";
-            "format-muted" = "󰝟  {volume}%";
-            "format-icons" = {
-              "default" = [
-                "󰕿 "
-                "󰖀 "
-                "󰕾 "
-              ];
-            };
-            "scroll-step" = 1;
-            "on-click" = "pavucontrol";
-          };
-
-          "battery" = {
-            "format" = "{icon} {capacity}%";
-            "format-charging" = "󱐋 {icon} {capacity}%";
-            "format-icons" = [
-              "󰂎"
-              "󱊡"
-              "󱊢"
-              "󱊣"
-            ];
-          };
-
-          "clock" = {
-            "format" = "  {:L%B %d, %R}";
-            "format-alt" = "  {:L%H:%M} ";
-            "tooltip-format" = "<tt><small>{calendar}</small></tt>";
-            # "locale" = "en_GB";
-            "calendar" = {
-              "mode" = "year";
-              "mode-mon-col" = 2;
-              "weeks-pos" = "left";
-              "on-scroll" = 1;
-              "on-click-right" = "mode";
-              "format" = {
-                "months" = "<span color='#ffead3'><b>{}</b></span>";
-                "days" = "<span color='#ecc6d9'><b>{}</b></span>";
-                "weeks" = "<span color='#99ffdd'><b>W{}</b></span>";
-                "weekdays" = "<span color='#ffcc66'><b>{}</b></span>";
-                "today" = "<span color='#ff6699'><b><u>{}</u></b></span>";
-              };
-            };
-            "actions" = {
-              "on-click-right" = "mode";
-              "on-click-left" = "mode";
-            };
           };
         };
       };
