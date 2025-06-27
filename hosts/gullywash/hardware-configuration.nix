63ee327b6478edf4b4e93be73969691f1221beeb
Guillaume Lagrange <guillaume@glagrange.eu>
2025-06-28 01:06:08 +0200

    fix(gullywash): add swapfile 😬

--- hosts/gullywash/hardware-configuration.nix
+++ hosts/gullywash/hardware-configuration.nix
@@ -55,7 +55,12 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 32768; # 32GB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
