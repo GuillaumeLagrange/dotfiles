21df84055dc142ba9f537e239b93510edb2de613
Guillaume Lagrange <guillaume@glagrange.eu>
2025-07-18 14:44:40 +0200

    feat(headless): simplify tsm

--- modules/headless/tmux.nix
+++ modules/headless/tmux.nix
@@ -5,17 +5,9 @@
   ...
 }:
 let
-  tmp_session_regex = "(.*-\\d+)|(\\d+)";
-
-  # Helper function to get sessions with optional regex exclusion
+  # Helper function to get sessions
   tmuxFzfGetSession = pkgs.writeShellScriptBin "tmux-fzf-get-session" ''
-    exclude_regex="$1"
     sessions=$(${pkgs.tmux}/bin/tmux list-sessions -F "#{session_name}" 2>/dev/null)
-
-    if [[ -n "$exclude_regex" ]]; then
-        sessions=$(echo "$sessions" | ${pkgs.gnugrep}/bin/grep -vP "$exclude_regex")
-    fi
-
     echo "$sessions" | ${pkgs.fzf}/bin/fzf --exit-0 --height 10
   '';
 
@@ -32,9 +24,6 @@ let
       - When called with an argument, attaches to the specified session if it exists,
         or creates a new session with the given name.
       - When called without an argument, prompts the user to select an existing session.
-        The selected session will be opened as an ephemeral session, sharing the windows
-        and panes with the main session, but with a decoupled focus.
-        This session is killed on detach.
 
     Parameters:
       SESSION_NAME  (optional) The name of the tmux session to create or attach to.
@@ -51,14 +40,14 @@ let
           exit 1
         fi
 
-        if [[ -n "$1" ]]; then
-          ${pkgs.tmux}/bin/tmux new-session -A -s "$1"
-          exit 0
+        if [[ -z "$1" ]]; then
+          session=$(${tmuxFzfGetSession}/bin/tmux-fzf-get-session)
+        else
+          session="$1"
         fi
 
-        session=$(${tmuxFzfGetSession}/bin/tmux-fzf-get-session "${tmp_session_regex}")
         if [[ -n "$session" ]]; then
-          ${pkgs.tmux}/bin/tmux new-session -t "$session" \; set-option destroy-unattached
+          ${pkgs.tmux}/bin/tmux new-session -A -s "$session"
         else
           echo "No session selected"
         fi
@@ -72,17 +61,7 @@ let
     fi
   '';
 
-  # Script to delete temporary sessions
-  deleteTmpSessionsScript = pkgs.writeShellScriptBin "tmux-delete-tmp-sessions" ''
-    ${pkgs.tmux}/bin/tmux list-sessions -F "#{session_name}" | ${pkgs.gnugrep}/bin/grep -P "${tmp_session_regex}" | while read -r session; do
-      ${pkgs.tmux}/bin/tmux kill-session -t "$session"
-      echo "Deleted tmux session: $session"
-    done
-  '';
-
   tmuxStart = pkgs.writeShellScriptBin "tmux-start" ''
-    # Remove leftover tmp sessions from the continuum save
-    # sed -i.bak '/^state\|^grouped_session/d' ~/.tmux/resurrect/last 
     tmux start-server
     echo "Tmux server started"
   '';
@@ -140,7 +119,6 @@ in
       tmuxFzfGetSession
       tsmScript
       tskScript
-      deleteTmpSessionsScript
       tmuxStart
     ];
   };
