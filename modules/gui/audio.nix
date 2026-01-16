568e4c53fc4f56f2568b9148a6d9ce39db1e7a3b
Guillaume Lagrange <guillaume@glagrange.eu>
2026-01-16 09:53:10 +0100

    --wip-- [skip ci]

--- modules/gui/audio.nix
+++ modules/gui/audio.nix
@@ -7,19 +7,27 @@ let
   # List of Bluetooth devices with their MAC address and priority
   # Higher priority = preferred device
   bluetoothDevices = [
-    { mac = "40:B3:FA:21:39:77"; priority = 2001; } # Airpods
-    { mac = "AC:80:0A:6F:E5:88"; priority = 2000; } # WH1000-XM5
+    {
+      # Airpods
+      mac = "40:B3:FA:21:39:77";
+      priority = 2001;
+    }
+    {
+      # WH1000-XM5
+      mac = "AC:80:0A:6F:E5:88";
+      priority = 2000;
+    }
   ];
 
   # Convert MAC address format from AA:BB:CC to AA_BB_CC
-  macToDeviceName = mac: "~bluez_card.${lib.replaceStrings [":"] ["_"] mac}";
+  macToDeviceName = mac: "bluez_output.${lib.replaceStrings [ ":" ] [ "_" ] mac}.a2dp-sink";
 
   # Generate a WirePlumber rule for a device
   mkDeviceRule = device: ''
     {
       matches = [
         {
-          device.name = "${macToDeviceName device.mac}"
+          node.name = "${macToDeviceName device.mac}"
         }
       ]
       actions = {
@@ -27,14 +35,13 @@ let
           device.priority = ${toString device.priority}
         }
       }
-    }
-  '';
+    }'';
 in
 {
   config = lib.mkIf config.gui.enable {
     xdg.configFile."wireplumber/wireplumber.conf.d/90-bluetooth-priority.conf".text = ''
       monitor.bluez.rules = [
-        ${lib.concatStringsSep "\n    " (map mkDeviceRule bluetoothDevices)}
+      ${lib.concatMapStringsSep ",\n" mkDeviceRule bluetoothDevices}
       ]
     '';
   };
